ca92ed22e60ff4add0f3fd5b5366de727cbb7485
ignzdave <david.sutanto@live.com>
2019-04-07 19:47:32 +0800

    Add files via upload
    upload test file for database connection

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:azuredavid.database.windows.net,1433; Database = latihanazure", "ignzdave", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+try {
+    $tableName = 'User';
+    
+    $query = "SELECT * FROM $tableName";
+    $stmt = $conn->query($query);
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    print_r($result);
+    
+    unset($stmt);
+    unset($conn);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
